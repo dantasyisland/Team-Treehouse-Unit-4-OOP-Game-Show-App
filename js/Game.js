57f5712a7d62978026b28aea60ed77e34fbdda0d
dantasyisland <dantothelastdrop@yahoo.com>
2021-12-26 23:00:25 -0600

    Made more headway

--- js/Game.js
+++ js/Game.js
@@ -13,22 +13,22 @@ class Game {
    * @return {array} An array of phrases that could be used in the game
    */
   createPhrases() {
-    const phrases = [{
-        phrase: "AND YEAH"
-      },
-      {
-        phrase: "AND YEAH"
-      },
-      {
-        phrase: "AND YEAH"
-      },
-      {
-        phrase: "AND YEAH"
-      },
-      {
-        phrase: "AND YEAH"
-      },
+    const phrases = [
+      new Phrase ('A winner is you'),
+      new Phrase ('Florna is an angel in disguise look into my eye'),
+      new Phrase ('Milk steak boiled over hard'),
+      new Phrase ('And it was also the night that the skeletons came to life'),
+      new Phrase ('There is no Dana only Zuul')
     ]
     return phrases;
   }
+
+  /**
+  * Selects random phrase from phrases property
+  * @return {Object} Phrase object chosen to be used
+  */
+  getRandomPhrase() {
+    const randomIndex = Math.floor(Math.random() * 5);
+    return this.phrases[randomIndex];
+  };
 }
